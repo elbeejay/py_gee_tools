b9861aa6203ef125267970cbe5fa2f35e5239afc
JayH <jayaram.hariharan@utexas.edu>
2020-05-24 17:07:47 -0400

    add pypi

--- docs/html/_sources/install/index.rst.txt
+++ docs/html/_sources/install/index.rst.txt
@@ -6,6 +6,15 @@ Installation Instructions
 
 .. note:: You must have access to *both* the `Python Google Earth Engine API <https://developers.google.com/earth-engine/python_install-conda#install_api>`_ and `Google Drive <https://www.google.com/drive/>`_ to use this package.
 
+Installation via PyPI
+---------------------
+
+1. From the command line:
+::
+
+   $ pip install py-gee-tools
+
+This will install the package from PyPI. Then from python ``import gee_python_tools`` will import the package.
 
 Installation from source
 ------------------------
@@ -20,4 +29,4 @@ Installation from source
 
    $ python setup.py install
 
-to install the package.
+to install the package. Then from python ``import gee_python_tools`` will import the package.
--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 from setuptools import setup
 
 setup(
-    name = 'gee_python_tools',
-    version = '1.0',
+    name = 'py_gee_tools',
+    version = '0.1',
     author = 'Jayaram Hariharan',
     author_email = 'jayaram.hariharan@utexas.edu',
-    description = ('Helper functions to work with the Google Earth Engine Python Library'),
-    keywords = 'remote sensing, landsat, satellite, Google, Earth Engine, Google Earth Engine, modis',
-    url = 'https://github.com/elbeejay/gee_python_tools',
+    description = ('Helper package to work with the Google Earth Engine Python Library'),
+    keywords = ['remote sensing, landsat, satellite, Google, Earth Engine, Google Earth Engine, modis', 'sentinel', 'sentinel-2'],
+    url = 'https://github.com/elbeejay/py_gee_tools',
     packages = ['gee_python_tools'],
-    long_description = 'See project webpage for details: https://github.com/elbeejay/py_gee_tools',
-    classifiers=['Programming Language :: Python :: 3.8'],
+    long_description = 'See project documentation for details: https://elbeejay.github.io/py_gee_tools',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+    python_requires='>=3.6',
     install_requires=['earthengine-api'],
 )
